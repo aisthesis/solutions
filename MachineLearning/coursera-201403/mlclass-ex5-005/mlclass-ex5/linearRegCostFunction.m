bf957e528a16cb744f1cb9a6ad32afc2a52d09bc
Marshall Farrier <info@marshallfarrier.com>
2014-04-26 14:48:34 -0700

    Regularized linear regression cost function

--- MachineLearning/coursera-201403/mlclass-ex5-005/mlclass-ex5/linearRegCostFunction.m
+++ MachineLearning/coursera-201403/mlclass-ex5-005/mlclass-ex5/linearRegCostFunction.m
@@ -19,8 +19,8 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
+regularization = (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
+J = sum(((X * theta) - y) .^ 2) / (2 * m) + regularization;
 
 
 
