ecace4fa1ee989cbd3ba3dfb121668427a682e1b
Marshall Farrier <info@marshallfarrier.com>
2014-04-29 01:16:51 -0700

    Email Preprocessing

--- MachineLearning/coursera-201403/mlclass-ex6-005/mlclass-ex6/processEmail.m
+++ MachineLearning/coursera-201403/mlclass-ex6-005/mlclass-ex6/processEmail.m
@@ -97,8 +97,10 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
+    index = lookup(vocabList, str, "m");
+    if index > 0
+        word_indices = [word_indices; index];
+    end
 
 
 
