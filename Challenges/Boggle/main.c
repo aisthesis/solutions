61c90118ce8867648a162c86ca1c7e5df6878bba
Marshall Farrier <info@marshallfarrier.com>
2014-04-29 18:06:07 -0700

    Start for C boggle solver.

---
+++ Challenges/Boggle/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Executable boggle solver
+ * Copyright (c) 2014 Marshall Farrier
+ * license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * @author Marshall Farrier
+ * @since 2014-04-29
+ */
+
+int main(void) {
+    printf("Hello world!\n");
+    return 0;
+}
--- README.md
+++ README.md
@@ -5,3 +5,13 @@ to remain private, as some solutions are for classes
 and contests.
 
 Started 2014-04-18
+
+Challenges
+---
+This directory contains solutions to programming challenges
+from diverse sources: 
+- competitions
+- practice problems
+- interview questions
+
+It supersedes similar public repositories.
