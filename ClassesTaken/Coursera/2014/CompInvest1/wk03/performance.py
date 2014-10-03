548f86e72af9986de37b1a3ec140586b73fe1aa6
Marshall Farrier <info@marshallfarrier.com>
2014-10-03 01:27:39 -0700

    Homework 1 for computational investing 1

---
+++ ClassesTaken/Coursera/2014/CompInvest1/wk03/performance.py
@@ -0,0 +1,56 @@
+'''
+Copyright (c) 2014 Marshall Farrier
+license http://opensource.org/licenses/gpl-license.php GNU Public License
+
+@author: Marshall Farrier
+@contact: marshalldfarrier@gmail.com
+@since: 2014-10-02
+@summary: Homework 1 (simulate function)
+'''
+
+# QSTK Imports
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+
+import datetime as dt
+import numpy as np
+import math
+
+def simulate(dt_start, dt_end, equities, allocations):
+    n_equities = len(equities)
+    # We need closing prices so the timestamp should be hours=16.
+    dt_timeofday = dt.timedelta(hours=16)
+
+    # Get a list of trading days between the start and the end.
+    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+
+    # Creating an object of the dataaccess class with Yahoo as the source.
+    c_dataobj = da.DataAccess('Yahoo')
+
+    # Keys to be read from the data, it is good to read everything in one go.
+    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+                    
+    # Reading the data, now d_data is a dictionary with the keys above.
+    # Timestamps and symbols are the ones that were specified before.
+    ldf_data = c_dataobj.get_data(ldt_timestamps, equities, ls_keys)
+    d_data = dict(zip(ls_keys, ldf_data))
+
+    # Filling the data for NAN
+    for s_key in ls_keys:
+        d_data[s_key] = d_data[s_key].fillna(method='ffill')
+        d_data[s_key] = d_data[s_key].fillna(method='bfill')
+        d_data[s_key] = d_data[s_key].fillna(1.0)
+
+    # Getting the numpy ndarray of close prices.
+    na_price = d_data['close'].values
+    na_normalized_price = na_price / na_price[0, :]
+    pf_daily_values = na_normalized_price.dot(allocations)
+    daily_returns = np.append([0], pf_daily_values[1:] / pf_daily_values[:-1] - 1)
+
+    volatility = np.std(daily_returns)
+    ave_daily_return = np.average(daily_returns)
+    sharpe = math.sqrt(252) * ave_daily_return / volatility
+    cumulative_return = pf_daily_values[-1]
+
+    return volatility, ave_daily_return, sharpe, cumulative_return
