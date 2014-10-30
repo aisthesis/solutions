43762d5b6f62bfc2d9d59adc8dde3eb08b144a88
Marshall Farrier <info@marshallfarrier.com>
2014-10-29 18:23:58 -0700

    Bollinger values

--- ClassesTaken/Coursera/2014/CompInvest1/wk07/hw05.py
+++ ClassesTaken/Coursera/2014/CompInvest1/wk07/hw05.py
@@ -17,23 +17,23 @@ import pandas as pd
 sys.path.append('../common')
 import market as mkt
 
-def show_bollinger(equity):
+def show_bollinger(equity, k=2.0):
     startdate = dt.datetime(2010, 1, 1)
     enddate = dt.datetime(2010, 12, 31)
-    data = get_data(equity, startdate, enddate)
+    data = get_data(equity, startdate, enddate, k)
     plot_bollinger(data, equity)
 
-def get_data(equity, startdate, enddate):
+def get_data(equity, startdate, enddate, k):
     data = mkt.get_market_data(startdate, enddate, [equity])['close']
     sigmas = pd.rolling_std(data, window=20).values.flatten()
     data.loc[:, 'SMA'] = pd.rolling_mean(data, window=20).values.flatten()
-    # TODO use 2 standard deviations:
-    # upper_band = data.loc[:, 'SMA'].values + 2 * sigmas
-    # lower_band = data.loc[:, 'SMA'].values - 2 * sigmas
-    upper_band = data.loc[:, 'SMA'].values + sigmas
-    lower_band = data.loc[:, 'SMA'].values - sigmas
+    upper_band = data.loc[:, 'SMA'].values + k * sigmas
+    lower_band = data.loc[:, 'SMA'].values - k * sigmas
     data.loc[:, 'upper'] = upper_band
     data.loc[:, 'lower'] = lower_band
+    data.loc[:, 'boll_val'] = (data.loc[:, equity].values.flatten() - \
+            data.loc[:, 'SMA'].values.flatten()) / sigmas
+    print data.ix[-5:, 'boll_val']
     return data
 
 def plot_bollinger(price_history, equity):
@@ -51,4 +51,4 @@ def plot_bollinger(price_history, equity):
     plt.show()
 
 if __name__ == '__main__':
-    show_bollinger(*sys.argv[1:])
+    show_bollinger(*sys.argv[1:], k=1.0)
