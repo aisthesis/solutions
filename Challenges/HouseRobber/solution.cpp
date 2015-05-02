483b40a1c07b8c48b5c0ca1fca2ee0ead12b3034
Marshall Farrier <info@marshallfarrier.com>
2015-05-01 23:03:03 -0700

    House robber problem.

---
+++ Challenges/HouseRobber/solution.cpp
@@ -0,0 +1,65 @@
+/**
+ * House Robber challenge from leetcode
+ * https://leetcode.com/problems/house-robber/
+ *
+ * Copyright (c) 2015 Marshall Farrier
+ * license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * @author Marshall Farrier
+ * @since 2015-05-01
+ */
+
+#include <vector>
+#include <iostream>
+
+class Solution {
+    private:
+        std::vector<int> best;
+    public:
+        int rob(std::vector<int> &nums) {
+            best = nums;
+            size_t len = nums.size();
+            if (len == 0) {
+                return 0;
+            }
+            else if (len > 1) {
+                solve(nums, len);
+            }
+            return best[len - 1]; 
+        }
+    private:
+        void solve(const std::vector<int> &nums, const size_t &len) {
+            bool robbed_last = true;
+            if (nums[1] < nums[0]) {
+                best[1] = best[0];
+                robbed_last = false;
+            }
+            for (int i = 2; i < len; ++i) {
+                if (robbed_last) {
+                    if (nums[i] + best[i - 2] > best[i - 1]) {
+                        // we can improve by robbing this house instead of previous
+                        best[i] = nums[i] + best[i - 2];
+                        robbed_last = true;
+                    }
+                    else {
+                        best[i] = best[i - 1];
+                        robbed_last = false;
+                    }
+                }
+                else {
+                    best[i] = best[i - 1] + nums[i];
+                    if (nums[i] > 0) {
+                        robbed_last = true;
+                    }
+                }
+            }
+        }
+};
+
+int main() {
+    Solution solution;
+    std::vector<int> test {5, 2, 7, 9, 3, 1};
+
+    std::cout << solution.rob(test) << std::endl;
+    return 0;
+}
