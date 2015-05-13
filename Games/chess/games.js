c3c4cffa270a3b530659ff1a5b5b137dc0725ec4
Marshall Farrier <info@marshallfarrier.com>
2015-05-13 16:12:23 -0700

    Petrosian-Gligorich 1970

--- Games/chess/games.js
+++ Games/chess/games.js
@@ -7,53 +7,64 @@
   'use strict';
   var games = [
     {
-      "white": "Paul Morphy",
-      "black": "Isouard, Duke Karl",
+      "white": "Morphy, Paul",
+      "black": ["Isouard", "Karl, Duke of Brunswick"],
       "year": "1858",
       "opening": "Philidor",
       "sources": ["https://youtu.be/vFnY77FmSeU"]
     },
     {
-      "white": "Paul Morphy",
-      "black": "Thomas Wilson Barnes",
+      "white": "Morphy, Paul",
+      "black": "Barnes, Thomas Wilson",
       "year": "1858",
       "opening": "Philidor",
-      "sources": ["ClassicGames (HIARCS)"]
+      "sources": ["ClassicGames (HIARCS), 5"]
     },
     {
-      "white": "K. Wygodschikoff",
-      "black": "Alexander Alekhine",
+      "white": "Wygodschikoff, K.",
+      "black": "Alekhine, Alexander",
       "year": "1909",
       "opening": "Spanish",
       "sources": ["Aljechin 1908-23, 34"]
     },
     {
-      "white": "A. Fleissig, G. Staehelin",
-      "black": "Alexander Alekhine",
+      "white": ["Fleissig, A.", "Staehelin, G."],
+      "black": "Alekhine, Alexander",
       "year": "1922",
       "opening": "Scotch",
       "sources": ["Aljechin 1908-23, 94"]
     },
     {
-      "white": "Bobby Fischer",
-      "black": "Mikhail Tal",
+      "white": "Fischer, Bobby",
+      "black": "Tal, Mikhail",
       "year": "1959",
       "opening": "Sicilian",
       "sources": ["Fischer Memorable, 17"]
     },
     {
-      "white": "Bobby Fischer",
-      "black": "Gligorich",
+      "white": "Fischer, Bobby",
+      "black": "Gligorich, Svetozar",
       "year": "1966",
       "opening": "Spanish",
       "sources": ["Fischer Memorable, 56"]
     },
     {
-      "white": "Mikhail Tal",
-      "black": "Viktor Korchnoi",
+      "white": "Petrosian, Tigran",
+      "black": "Gligorich, Svetozar",
+      "year": "1970",
+      "opening": "Kings Indian",
+      "sources": [
+        "Svetozar_Gligoric (HIARCS), 2160",
+        "https://www.youtube.com/watch?v=HFIOzJwUK4o"
+      ]
+    },
+    {
+      "white": "Tal, Mikhail",
+      "black": "Korchnoi, Viktor",
       "year": "1987",
       "opening": "Spanish",
-      "sources": ["Mikhail__Tal (HIARCS)"]
+      "sources": ["Mikhail__Tal (HIARCS), 168"],
+      "transposesTo": ["1.e4,e5 2.Nf3,Nc6 3.Bb5,Nf6 4.d4,Ne4: 5.0-0,a6 6.Ba4"]
     }
   ];
   return  games;
