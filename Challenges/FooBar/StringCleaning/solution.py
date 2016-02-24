529769c6594b25112007ad43adee2e91b3668c4d
Marshall Farrier <info@marshallfarrier.com>
2016-02-23 16:18:32 -0800

    Solution to String Cleaner with memoization

--- Challenges/FooBar/StringCleaning/solution.py
+++ Challenges/FooBar/StringCleaning/solution.py
@@ -1,40 +1,64 @@
 def answer(chunk, word):
-    # (recursive)
-    if chunk.find(word) < 0:
-        return chunk
-    residues = _find_all_residues(chunk, word)
-    best_ans = '{'
-    for residue in residues:
-        curr_ans = answer(residue, word)
-        if curr_ans < best_ans:
-            best_ans = curr_ans
-    return best_ans
+    cleaner = StringCleaner(word)
+    return cleaner.clean(chunk)
+
+class StringCleaner(object):
+
+    def __init__(self, word):
+        self.word = word
+        self.word_len = len(word)
+        # for memoization
+        self.answers = {}
+        self.worst_answer = ''
+
+    def clean(self, chunk):
+        self.answers = {}
+        self.worst_answer = chunk
+        return self._clean(chunk)
+
+    def _clean(self, chunk):
+        # recursive
+        if chunk in self.answers:
+            return self.answers[chunk]
+        if chunk.find(self.word) < 0:
+            self.answers[chunk] = chunk
+            return chunk
+        residues = self._find_all_residues(chunk)
+        best_ans = self.worst_answer
+        for residue in residues:
+            curr_ans = self._clean(residue)
+            if len(curr_ans) < len(best_ans):
+                best_ans = curr_ans
+            elif curr_ans < best_ans:
+                best_ans = curr_ans
+        self.answers[chunk] = best_ans
+        return best_ans
     
-def _find_all_residues(chunk, word):
-    indices = _find_all_indices(chunk, word)
-    residues = []
-    word_len = len(word)
-    for ix in indices:
-        residues.append(_get_residue(chunk, ix, word_len))
-    return residues
+    def _find_all_residues(self, chunk):
+        indices = self._find_all_indices(chunk)
+        residues = []
+        for ix in indices:
+            residues.append(self._get_residue(chunk, ix))
+        return residues
 
-def _find_all_indices(chunk, word):
-    i = 0
-    indices = []
-    last_i = len(chunk) - len(word)
-    while i <= last_i:
-        next_ix = chunk.find(word, i)
-        if next_ix < 0:
-            break
-        indices.append(next_ix)
-        i = next_ix + 1
-    return indices
+    def _find_all_indices(self, chunk):
+        i = 0
+        indices = []
+        last_i = len(chunk) - self.word_len
+        while i <= last_i:
+            next_ix = chunk.find(self.word, i)
+            if next_ix < 0:
+                break
+            indices.append(next_ix)
+            i = next_ix + 1
+        return indices
 
-def _get_residue(chunk, ix, word_len):
-    return chunk[0:ix] + chunk[ix + word_len:]
+    def _get_residue(self, chunk, ix):
+        return chunk[0:ix] + chunk[ix + self.word_len:]
 
 if __name__ == '__main__':
-    chunks = ["lololololo", "goodgooogoogfogoood"]
-    words = ["lol", "goo"]
+    # memoization passes tests
+    chunks = ['aabb', "lololololo", "goodgooogoogfogoood"]
+    words = ['ab', "lol", "goo"]
     for i in range(len(chunks)):
-        print(answer(chunks[i], words[i]))
+        print("'{}'".format(answer(chunks[i], words[i])))
