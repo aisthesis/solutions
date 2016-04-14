330a6aeb2c010bd366a3e0e7f587baa45f806d01
Marshall Farrier <info@marshallfarrier.com>
2016-04-13 23:27:42 -0700

    Chess update.

--- Games/chess/white/Sicilian/Najdorf.js
+++ Games/chess/white/Sicilian/Najdorf.js
@@ -8,12 +8,19 @@
   var repertoire = {
     '1.e4,c5 2.Nf3,d6 3.d4,cd4: 4.Nd4:,Nf6 5.Nc3,a6 6.Bc4,e6 7.Bb3': {
       'notes': 'Lipnitzky attack (6.Bc4)',
-      '7..b5': {},
-      '7..Be7': {},
-      '7..Nc6': {},
+      '7..b5': {
+        'Fischer_Tal1959': ['Fischer60, 17'],
+        'Kasparov_Gelfand1991': []
+      },
       '7..Nbd7': {
         'Fischer_Bednarski1966': ['Fischer60, 55']
-      }
+      },
+      '7..Be7': {
+        'Rublevsky_Ivanchuk2007': []
+      },
+      '7..Nc6': {
+        'Short_Kasparov1993': []
+      },
     }
   };
 
