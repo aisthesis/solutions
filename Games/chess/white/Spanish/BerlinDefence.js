dfd330c95d24bdc705b77fce9d94fe543c7b1ab2
Marshall Farrier <info@marshallfarrier.com>
2016-04-16 19:28:10 -0700

    Missing "

--- Games/chess/white/Spanish/BerlinDefence.js
+++ Games/chess/white/Spanish/BerlinDefence.js
@@ -10,7 +10,7 @@
     "1.e4,e5 2.Nf3,Nc6 3.Bb5, Nf6": {
       "4.0-0,Ne4: 5.d4": {
         "5..Nd6": {
-          "Fischer_Fuller1963": ["http://www.365chess.com/view_game.php?g=2573670]
+          "Fischer_Fuller1963": ["http://www.365chess.com/view_game.php?g=2573670"]
         },
         "5..a6": "BerlinRosenthal",
         "5..Be7 6.Qe2,Nd6 7.Bc6:,bc6: 8.de5:,Nb7 9.Nd4": {
