20bbb9d25a6c94c45d6745b810f9f6e2ceef4fe0
Marshall Farrier <marshalldfarrier@gmail.com>
2018-04-13 16:18:40 -0700

    Time series analysis wk 5. ARIMA.

---
+++ ClassesTaken/Coursera/2018/TimeSeries/wk05.R
@@ -0,0 +1,31 @@
+GetArimaData <- function(model, n=2000, seed=FALSE) {
+    if (seed) {
+        set.seed(seed)
+    }
+    return(arima.sim(model, n))
+}
+
+ExamineSeries <- function(data, xlim=NULL) {
+    centered.data <- data - mean(data)
+    par(mfrow=c(3, 1))
+    if (length(xlim)) {
+        plot(data, main='Raw data', col='blue', lwd=3, xlim=xlim)
+    }
+    else {
+        plot(data, main='Raw data', col='blue', lwd=3)
+    }
+    acf(centered.data, main='ACF (centered)', col='red', lwd=3)
+    pacf(centered.data, main='PACF (centered)', col='green', lwd=3)
+}
+
+ShowAic <- function(data, ar.max.lag=3, ma.max.lag=2) {
+    for (ar.lag in 0:ar.max.lag) {
+        for (ma.lag in 0:ma.max.lag) {
+            if (ar.lag == 0 && ma.lag == 0) {
+                next
+            }
+            print(paste('ar(', ar.lag, '), ma(', ma.lag, '): ', AIC(arima(data, order=c(ar.lag, 0, ma.lag))), sep=''))
+        }
+    }
+}
+
